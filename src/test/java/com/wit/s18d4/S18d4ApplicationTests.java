2cab1734208f0c28575a09f7f95dc4c41610f2da
emracatal <134175723+emracatal@users.noreply.github.com>
2023-12-21 13:41:02 +0300

    initial commit

---
+++ src/test/java/com/wit/s18d4/S18d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.s18d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
